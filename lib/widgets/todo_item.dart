e890dc2154d25dc15d4de4ccf472de9fe4d0cfba
Cynthia Franco <cynthia.raphaella@gmail.com>
2019-03-31 22:58:27 -0300

    Add state to the checkbox

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -1,18 +1,50 @@
 import 'package:flutter/material.dart';
 
-class TodoItem extends StatelessWidget {
+class TodoItem extends StatefulWidget {
   @override
-  Widget build(BuildContext context) => Card(
+  State<StatefulWidget> createState() => TodoItemState();
+}
+
+class TodoItemState extends State<TodoItem> {
+  final int _animationDuration = 500;
+  double _opacityLevel = 1.0;
+  bool _value = false;
+  FontStyle _fontStyle = FontStyle.normal;
+
+  @override
+  Widget build(BuildContext context) => AnimatedOpacity(
+        duration: Duration(milliseconds: _animationDuration),
+        opacity: _opacityLevel,
+        child: Card(
           child: Row(
             children: <Widget>[
               Checkbox(
-                onChanged: (value) => print(value),
-                value: true,
+                onChanged: (v) => _changeCheckBox(v),
+                value: _value,
               ),
               Text(
                 'Texto',
+                style: TextStyle(fontStyle: _fontStyle),
               )
             ],
-          )
-    );
+          ),
+        ),
+      );
+
+  _changeCheckBox(bool newValue) {
+    setState(() {
+      _value = newValue;
+      _changeFontStyle();
+      _changeOpacity();
+    });
+  }
+
+  _changeOpacity() {
+    setState(() => _opacityLevel = _opacityLevel == 0.5 ? 1.0 : 0.5);
+  }
+
+  _changeFontStyle() {
+    setState(() => _fontStyle =
+        _fontStyle == FontStyle.normal ? FontStyle.italic : FontStyle.normal);
+  }
 }
