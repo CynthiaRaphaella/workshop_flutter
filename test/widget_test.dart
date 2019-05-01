12c1fc06627a790a4f46f83f8c16ccf2a123148b
Cynthia Franco <cynthia.raphaella@gmail.com>
2019-05-01 14:40:50 -0300

    Add tests for checkbox state change

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:workshop_flutter/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Text is still being displayed', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
@@ -24,5 +24,38 @@ void main() {
 
     // Verify that the todo widget is still displayed
     expect(find.text('Texto'), findsWidgets);
+  });
+
+  testWidgets('Update opacity and style when checkbox is checked', (WidgetTester tester) async {
+    final SemanticsHandle handle = tester.ensureSemantics();
+
+    await tester.pumpWidget(MyApp());
+
+    expect(tester.getSemantics(find.byType(Checkbox).first), matchesSemantics(
+      hasCheckedState: true,
+      hasEnabledState: true,
+      isEnabled: true,
+      hasTapAction: true,
+      isChecked: false
+    ));
+
+    expect(tester.firstWidget<AnimatedOpacity>(find.byType(AnimatedOpacity)).opacity, 1);
+    expect(tester.firstWidget<Text>(find.byType(Text)).style.fontStyle, FontStyle.normal);
+
+    await tester.tap(find.byType(Checkbox).first);
+    await tester.pumpAndSettle();
+
+    expect(tester.firstWidget<AnimatedOpacity>(find.byType(AnimatedOpacity)).opacity, 0.5);
+    expect(tester.firstWidget<Text>(find.byType(Text)).style.fontStyle, FontStyle.italic);
+
+    expect(tester.getSemantics(find.byType(Checkbox).first), matchesSemantics(
+      hasCheckedState: true,
+      hasEnabledState: true,
+      isEnabled: true,
+      hasTapAction: true,
+      isChecked: true
+    ));
+
+    handle.dispose();
   });
 }
