36078b1723f0ed569869850b67d3bdfc03d92b13
Cynthia Franco <cynthia.raphaella@gmail.com>
2019-05-01 22:03:54 -0300

    Action button adds a new item

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:workshop_flutter/widgets/todo_item.dart';
 import 'package:workshop_flutter/widgets/todo_list.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  final List<TodoItem> items = new List(); 
+  @override
+  State<StatefulWidget> createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(title: Text("To do!")),
-        body: TodoList(),
+        body: TodoList(items: widget.items),
         floatingActionButton: FloatingActionButton(
-          onPressed: () => print('New todo'),
+          onPressed: () => setState(() { widget.items.add(new TodoItem()); }),
           child: Icon(Icons.add),
         ),
       );
--- lib/widgets/todo_list.dart
+++ lib/widgets/todo_list.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'todo_item.dart';
 
 class TodoList extends StatelessWidget {
+  TodoList({Key key, this.items}) : super(key: key);
+
+  final List<TodoItem> items;
+
   @override
   Widget build(BuildContext context) => Center(
-          child: ListView(
-        children: <Widget>[TodoItem(), TodoItem()],
-      ));
+            child: Column(children: items)
+          );
 }
+
+  
